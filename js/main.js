add862ed5934436677b14e4b5f7e6d7132997064
vivekmurali2k <45914640+vivekmurali2k@users.noreply.github.com>
2020-03-11 19:19:54 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -140,7 +140,7 @@ jQuery(document).ready(function($) {
   date: '03/11/2020 12:00:00',
   offset: +10
 }, function () {
-  //alert('WOOT WOOT, done!');
+  alert('Choose Your Problem Statement');
 });
 
 });
